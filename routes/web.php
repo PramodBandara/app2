cd0d72365ed82aaf9a5e6d5876512ef28ae97ac0
Pramod Bandara <rupikabalasuriya123@gmail.com>
2025-06-26 23:41:32 +0530

    Add routes for about, contact, work, and center pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app2');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/work', function () {
+    return view('work');
+});
+Route::get('/center', function () {
+    return view('center');
+});
